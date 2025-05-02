e70709defda04bf10bbd0e62d1087873cb7eb499
lolopindik <dimaluka2014@gmail.com>
2025-05-02 21:41:14 +0300

    work on layout is in progress

--- lib/presentation/pages/form_pages/initial_page.dart
+++ lib/presentation/pages/form_pages/initial_page.dart
@@ -5,40 +5,58 @@ import 'package:pet_care/logic/riverpod/pet_form.dart';
 
 class InitialPage {
   Widget build(BuildContext context, WidgetRef ref) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    // final screenWidth = MediaQuery.of(context).size.width;
+
     final stepIncrement = ref.read(stepProvider).incrementStep;
 
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              height: screenHeight * 0.6,
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(20)),
-            ),
-            Gap(60),
-            ElevatedButton(
-              //todo add logic to check if fields are filled
-              onPressed: (){
-                stepIncrement();
-              },
-              style: ButtonStyle(),
-              child: Text('Continue'),
-            ),
-            // TextButton(
-            //     onPressed: () {},
-            //     child: Text(
-            //       'Skip for now',
-            //       style: Theme.of(context)
-            //           .textTheme
-            //           .bodySmall
-            //           ?.copyWith(fontSize: 12),
-            //     )),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(flex: 1, 
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 600),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)
+                  )
+                ),
+                )),
+              Expanded(flex: 7,
+               child: Container(
+                constraints: BoxConstraints(maxWidth: 600),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft:Radius.circular(20),
+                    bottomRight: Radius.circular(20)
+                  )
+                ),
+                )),
+              Gap(60),
+              ElevatedButton(
+                //todo add logic to check if fields are filled
+                onPressed: () {
+                  stepIncrement();
+                },
+                style: ButtonStyle(),
+                child: Text('Continue'),
+              ),
+              // TextButton(
+              //     onPressed: () {},
+              //     child: Text(
+              //       'Skip for now',
+              //       style: Theme.of(context)
+              //           .textTheme
+              //           .bodySmall
+              //           ?.copyWith(fontSize: 12),
+              //     )),
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/widgets/pet_form_apppbar.dart
+++ lib/presentation/widgets/pet_form_apppbar.dart
@@ -4,56 +4,69 @@ import 'package:pet_care/logic/riverpod/pet_form.dart';
 import 'package:pet_care/logic/theme/theme_constants.dart';
 
 class PetFormApppbar {
-  PreferredSizeWidget? build(BuildContext context, currentStep, maxSteps, ref){
-
+  PreferredSizeWidget? build(BuildContext context, currentStep, maxSteps, ref) {
     final stepDecrement = ref.read(stepProvider).decrementStep;
 
     Map<int, String> label = {
-      1: 'Add pet profile',
+      1: 'Tell us about your pet',
       2: 'Find breed of pet ',
       3: '333',
       4: '444'
     };
-    
+
     return AppBar(
-        leading: (currentStep > 1) ? IconButton(onPressed: ()=> stepDecrement(), icon: Icon(Icons.arrow_back_ios_new)) : null,
-        backgroundColor: const Color.fromARGB(255, 217, 217, 217),
-        toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-        centerTitle: true,
-        title: Column(children: <Widget>[
-          Text('Add pet profile', style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            fontWeight: FontWeight.bold,
-            fontSize: 22
-          ),),
-          Text('${label[currentStep]}', style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            color: const Color.fromARGB(255, 112, 112, 112),
-            fontWeight: FontWeight.bold,
-          ),)
+      leading: (currentStep > 1)
+          ? IconButton(
+              onPressed: () => stepDecrement(),
+              icon: Icon(Icons.arrow_back_ios_new))
+          : null,
+      backgroundColor: const Color.fromARGB(255, 217, 217, 217),
+      toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+      centerTitle: true,
+      title: Column(
+        children: <Widget>[
+          Text(
+            'Add pet profile',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge
+                ?.copyWith(fontWeight: FontWeight.bold, fontSize: 22),
+          ),
+          Text(
+            '${label[currentStep]}',
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: const Color.fromARGB(255, 112, 112, 112),
+                  fontWeight: FontWeight.bold,
+                ),
+          )
         ],
       ),
       actions: [
         Padding(
           padding: const EdgeInsets.only(right: 20),
-          child: Text('Step $currentStep/$maxSteps', style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            fontSize: 14,
-              color: const Color.fromARGB(255, 112, 112, 112),
-              fontWeight: FontWeight.bold,
-            ),),
-        ),
-      ],
-       bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(10),
-          child: Container(
-            margin: EdgeInsets.only(bottom: 20, left: 20, right: 15),
-            child: TitledProgressBar(
-              maxSteps: maxSteps,
-              currentStep: currentStep,
-              progressColor: LightModeColors.secondaryColor,
-              backgroundColor: LightModeColors.primaryColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
+          child: Text(
+            'Step $currentStep/$maxSteps',
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontSize: 14,
+                  color: const Color.fromARGB(255, 112, 112, 112),
+                  fontWeight: FontWeight.bold,
+                ),
           ),
         ),
-      );
+      ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(10),
+        child: Container(
+          margin: EdgeInsets.only(bottom: 20, left: 20, right: 15),
+          child: TitledProgressBar(
+            maxSteps: maxSteps,
+            currentStep: currentStep,
+            progressColor: LightModeColors.secondaryColor,
+            backgroundColor: LightModeColors.primaryColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
   }
 }
